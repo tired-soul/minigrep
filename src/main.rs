52f20ae0d87d7f41a45c9b46e2d35a34e4e16326
Nithish <nithishk.ch@gmail.com>
2023-03-12 00:29:56 +0530

    Before refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -8,4 +9,9 @@ fn main() {
     //print query and filename
     println!("Searching for \"{}\"", query);
     println!("In file \"{}\"", filename);
+
+    let content = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("\nWith text:\n{} ", content);
 }

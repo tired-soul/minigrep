4e0767a47078cd610ae230131011471f557d19f0
Nithish <nithishk.ch@gmail.com>
2023-03-12 01:13:41 +0530

    Refactored parsing and configuring

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
+use minigrep::Config;
 use std::env;
 use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-
+    let config = Config::build(&args).unwrap();
     //print query and filename
-    println!("Searching for \"{}\"", query);
-    println!("In file \"{}\"", filename);
+    println!("Searching for \"{}\"", config.query);
+    println!("In file \"{}\"", config.filename);
 
-    let content = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    let content =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("\nWith text:\n{} ", content);
 }
